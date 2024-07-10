84496f848055e1b437a2bb4ba86966ce531c1458
Mario Albornoz <mario.alb.andino@gmail.com>
2024-07-10 20:00:41 +0200

    cleaned up unused icon files

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,27 +1,16 @@
 import css from './css.svg'
-import express from './express.svg'
 import git from './git.svg'
 import github from './github.svg'
 import html from './html.svg'
 import javascript from './javascript.svg'
 import mariadb from './mariadb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
 import nextjs from './nextjs.svg'
 import nodejs from './nodejs.svg'
 import react from './react.svg'
-import redux from './redux.svg'
 import python from './python.svg'
 import tailwindcss from './tailwindcss.svg'
 import typescript from './typescript.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
 import pdf from './pdf.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
 import arrow from './arrow.svg'
 import contact from './contact.svg'
 import fill from './fill.svg'
@@ -32,29 +21,18 @@ import arrowProjects from './arrowprojects.svg'
 
 export {
     css,
-    express,
     git,
     github,
     html,
     javascript,
     mariadb,
-    motion,
-    mui,
     nextjs,
     nodejs,
     react,
-    redux,
     python,
     tailwindcss,
     typescript,
-    linkedin,
-    twitter,
     pdf,
-    estate,
-    pricewise,
-    snapgram,
-    summiz,
-    threads,
     arrow,
     contact,
     fill,
